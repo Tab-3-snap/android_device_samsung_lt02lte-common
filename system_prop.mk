1979b0de8fcdedff3ac5ede3e1236c37e3e38ac3
deadman96385 <seanhoyt963@gmail.com>
2017-04-03 00:56:31 -0500

    Enable mobile data by default

--- system_prop.mk
+++ system_prop.mk
@@ -4,7 +4,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.android.mobiledata=false \
     ro.telephony.ril_class=SerranoRIL \
     ro.ril.telephony.mqanelements=6 \
     persist.radio.add_power_save=1

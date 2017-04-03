fca6a611d2ebc8cce752915fdc7a36929ca45441
deadman96385 <seanhoyt963@gmail.com>
2017-04-03 00:48:55 -0500

    Set the proper dalvik heap config

--- lt02ltexx-common.mk
+++ lt02ltexx-common.mk
@@ -145,4 +145,4 @@ $(call inherit-product, device/samsung/lt02ltexx-common/system_prop.mk)
 $(call inherit-product, device/samsung/msm8930-common/msm8930.mk)
 
 # call dalvik heap config
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)

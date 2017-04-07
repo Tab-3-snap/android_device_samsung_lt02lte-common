34426980718719213d87f823b5f64c67ae6ca0ed
deadman96385 <seanhoyt963@gmail.com>
2017-04-07 16:51:43 -0500

    Update ril class
    Based on FaArlsH's work

--- ril/SerranoRIL.java
+++ ril/SerranoRIL.java
@@ -24,7 +24,6 @@ import android.telephony.Rlog;
 import android.os.AsyncResult;
 import android.os.Message;
 import android.os.Parcel;
-import android.os.SystemProperties;
 import android.telephony.ModemActivityInfo;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.SignalStrength;
@@ -47,9 +46,6 @@ public class SerranoRIL extends RIL {
     private static final int RIL_UNSOL_AM = 11010;
     private static final int RIL_UNSOL_WB_AMR_STATE = 11017;
     private static final int RIL_UNSOL_RESPONSE_HANDOVER = 11021;
-    private static final int RIL_UNSOL_ON_SS_SAMSUNG = 1040;
-    private static final int RIL_UNSOL_STK_CC_ALPHA_NOTIFY_SAMSUNG = 1041;
-    private static final int RIL_UNSOL_UICC_SUBSCRIPTION_STATUS_CHANGED_SAMSUNG = 11031;
 
     public SerranoRIL(Context context, int preferredNetworkType, int cdmaSubscription) {
         this(context, preferredNetworkType, cdmaSubscription, null);
@@ -236,79 +232,6 @@ public class SerranoRIL extends RIL {
     }
 
     @Override
-    protected RILRequest
-    processSolicited (Parcel p, int type) {
-        int serial, error;
-        boolean found = false;
-        int dataPosition = p.dataPosition(); // save off position within the Parcel
-        serial = p.readInt();
-        error = p.readInt();
-        RILRequest rr = null;
-        /* Pre-process the reply before popping it */
-        synchronized (mRequestList) {
-            RILRequest tr = mRequestList.get(serial);
-            if (tr != null && tr.mSerial == serial) {
-                if (error == 0 || p.dataAvail() > 0) {
-                    try {switch (tr.mRequest) {
-                            /* Get those we're interested in */
-                        case RIL_REQUEST_DATA_REGISTRATION_STATE:
-                            rr = tr;
-                            break;
-                    }} catch (Throwable thr) {
-                        // Exceptions here usually mean invalid RIL responses
-                        if (tr.mResult != null) {
-                            AsyncResult.forMessage(tr.mResult, null, thr);
-                            tr.mResult.sendToTarget();
-                        }
-                        return tr;
-                    }
-                }
-            }
-        }
-        if (rr == null) {
-            /* Nothing we care about, go up */
-            p.setDataPosition(dataPosition);
-            // Forward responses that we are not overriding to the super class
-            return super.processSolicited(p, type);
-        }
-        rr = findAndRemoveRequestFromList(serial);
-        if (rr == null) {
-            return rr;
-        }
-        Object ret = null;
-        if (error == 0 || p.dataAvail() > 0) {
-            switch (rr.mRequest) {
-                case RIL_REQUEST_DATA_REGISTRATION_STATE: ret = responseDataRegistrationState(p); break;
-                default:
-                    throw new RuntimeException("Unrecognized solicited response: " + rr.mRequest);
-            }
-            //break;
-        }
-        if (RILJ_LOGD) riljLog(rr.serialString() + "< " + requestToString(rr.mRequest)
-                               + " " + retToString(rr.mRequest, ret));
-        if (rr.mResult != null) {
-            AsyncResult.forMessage(rr.mResult, ret, null);
-            rr.mResult.sendToTarget();
-        }
-        return rr;
-    }
-
-    private Object
-    responseDataRegistrationState(Parcel p) {
-        String response[] = (String[])responseStrings(p);
-        /* DANGER WILL ROBINSON
-         * In some cases from Vodaphone we are receiving a RAT of 102
-         * while in tunnels of the metro. Lets Assume that if we
-         * receive 102 we actually want a RAT of 2 for EDGE service */
-        if (response.length > 4 &&
-            response[0].equals("1") &&
-            response[3].equals("102")) {
-            response[3] = "2";
-        }
-        return response;
-    }
-
-    @Override
     protected void
     processUnsolicited (Parcel p, int type) {
         Object ret;
@@ -330,18 +253,6 @@ public class SerranoRIL extends RIL {
                 break;
             case RIL_UNSOL_RESPONSE_HANDOVER:
                 ret = responseVoid(p);
-                break;
-            case RIL_UNSOL_ON_SS_SAMSUNG:
-                p.setDataPosition(dataPosition);
-                p.writeInt(RIL_UNSOL_ON_SS);
-                break;
-            case RIL_UNSOL_STK_CC_ALPHA_NOTIFY_SAMSUNG:
-                p.setDataPosition(dataPosition);
-                p.writeInt(RIL_UNSOL_STK_CC_ALPHA_NOTIFY);
-                break;
-            case RIL_UNSOL_UICC_SUBSCRIPTION_STATUS_CHANGED_SAMSUNG:
-                p.setDataPosition(dataPosition);
-                p.writeInt(RIL_UNSOL_UICC_SUBSCRIPTION_STATUS_CHANGED);
                 break;
             default:
                 // Rewind the Parcel
@@ -366,6 +277,26 @@ public class SerranoRIL extends RIL {
         rr.mParcel.writeInt(0);        // Unknown
 
         if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
+
+        send(rr);
+    }
+
+    @Override
+    public void setInitialAttachApn(String apn, String protocol, int authType, String username,
+            String password, Message result) {
+        RILRequest rr = RILRequest.obtain(RIL_REQUEST_SET_INITIAL_ATTACH_APN, null);
+
+        if (RILJ_LOGD) riljLog("Set RIL_REQUEST_SET_INITIAL_ATTACH_APN");
+
+        rr.mParcel.writeString(apn);
+        rr.mParcel.writeString(protocol);
+        rr.mParcel.writeInt(authType);
+        rr.mParcel.writeString(username);
+        rr.mParcel.writeString(password);
+
+        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
+                + ", apn:" + apn + ", protocol:" + protocol + ", authType:" + authType
+                + ", username:" + username + ", password:" + password);
 
         send(rr);
     }
@@ -411,6 +342,9 @@ public class SerranoRIL extends RIL {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void getCellInfoList(Message result) {
         riljLog("getCellInfoList: not supported");
@@ -422,6 +356,9 @@ public class SerranoRIL extends RIL {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setCellInfoListRate(int rateInMillis, Message response) {
         riljLog("setCellInfoListRate: not supported");
@@ -444,27 +381,34 @@ public class SerranoRIL extends RIL {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setDataAllowed(boolean allowed, Message result) {
-        if (SystemProperties.get("persist.radio.multisim.config").equals("dsds")) {
-            int req = 123;
-            RILRequest rr;
-            if (allowed) {
-                req = 116;
-                rr = RILRequest.obtain(req, result);
-            } else {
-                rr = RILRequest.obtain(req, result);
-                rr.mParcel.writeInt(1);
-                rr.mParcel.writeInt(allowed ? 1 : 0);
-            }
-
-            send(rr);
+        riljLog("setDataAllowed: not supported");
+        if (result != null) {
+            CommandException ex = new CommandException(
+                CommandException.Error.REQUEST_NOT_SUPPORTED);
+            AsyncResult.forMessage(result, null, ex);
+            result.sendToTarget();
         }
     }
 
     @Override
     public void startLceService(int reportIntervalMs, boolean pullMode, Message response) {
         riljLog("startLceService: not supported");
+        if (response != null) {
+            CommandException ex = new CommandException(
+                CommandException.Error.REQUEST_NOT_SUPPORTED);
+            AsyncResult.forMessage(response, null, ex);
+            response.sendToTarget();
+        }
+    }
+
+    @Override
+    public void iccOpenLogicalChannel(String AID, Message response) {
+        riljLog("iccOpenLogicalChannel: not supported");
         if (response != null) {
             CommandException ex = new CommandException(
                 CommandException.Error.REQUEST_NOT_SUPPORTED);
@@ -484,21 +428,5 @@ public class SerranoRIL extends RIL {
             AsyncResult.forMessage(response, null, ex);
             response.sendToTarget();
         }
-    }
-
-    @Override
-    public void setUiccSubscription(int appIndex, boolean activate, Message result) {
-        // Note: This RIL request is also valid for SIM and RUIM (ICC card)
-        RILRequest rr = RILRequest.obtain(115, result);
-
-        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
-                + " appIndex: " + appIndex + " activate: " + activate);
-
-        rr.mParcel.writeInt(mInstanceId);
-        rr.mParcel.writeInt(appIndex);
-        rr.mParcel.writeInt(mInstanceId);
-        rr.mParcel.writeInt(activate ? 1 : 0);
-
-        send(rr);
     }
 }

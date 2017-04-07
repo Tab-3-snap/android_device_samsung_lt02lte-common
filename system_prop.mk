27e4b5d45f349a04871cf470692cb124d8a6a7af
deadman96385 <seanhoyt963@gmail.com>
2017-04-07 17:39:34 -0500

    Rotate the screen 270 degrees

--- system_prop.mk
+++ system_prop.mk
@@ -34,7 +34,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160 \
     ro.opengles.version=196608 \
     ro.qualcomm.cabl=0 \
-    ro.hwui.text_large_cache_height=2048
+    ro.hwui.text_large_cache_height=2048 \
+    ro.sf.hwrotation=270
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \

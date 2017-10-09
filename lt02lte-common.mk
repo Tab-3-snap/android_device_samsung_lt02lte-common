0931abb25909eb4f894a5beacab2c7052077d797
Arne Coucheron <arco68@gmail.com>
2017-10-09 02:59:59 +0200

    serrano-common: Add file for setting debug properties
    To make it easier for working on bringups. Not used by default.
    
    Change-Id: I059f609756f21616fbb0e9a3e2c12661d583013c

--- lt02lte-common.mk
+++ lt02lte-common.mk
@@ -146,6 +146,9 @@ PRODUCT_PACKAGES += \
 # call common lt02lte system props
 $(call inherit-product, device/samsung/lt02lte-common/system_prop.mk)
 
+# call common serrano system debug props
+#$(call inherit-product, device/samsung/serrano-common/system_prop_debug.mk)
+
 # call common msm8930
 $(call inherit-product, device/samsung/msm8930-common/msm8930.mk)
 

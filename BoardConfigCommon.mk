LineageOS/android_device_samsung_serrano-common

serrano-common: Use COMMON_PATH variable
Change-Id: I57083626ffaf4629d13b132cbee1e3b9a08fe3c5

BoardConfigCommon.mk
@@ -21,6 +21,8 @@
 # definition file).
 #
 
+COMMON_PATH := device/samsung/serrano-common
+
 # Inherit from common msm8930
 -include device/samsung/msm8930-common/BoardConfigCommon.mk
 
@@ -44,7 +46,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
 # Recovery
 LZMA_RAMDISK_TARGETS := recovery
-TARGET_RECOVERY_FSTAB := device/samsung/serrano-common/rootdir/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/fstab.qcom
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -59,7 +61,7 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/serrano-common/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth
 
 # FM
 AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
@@ -86,8 +88,8 @@ BOARD_RIL_CLASS := ../../../device/samsung/serrano-common/ril/
 BOARD_USES_QC_TIME_SERVICES := true
 
 # CMHW
-BOARD_HARDWARE_CLASS += device/samsung/serrano-common/cmhw
+BOARD_HARDWARE_CLASS += $(COMMON_PATH)/cmhw
 
 ifeq ($(WITH_TWRP),true)
--include device/samsung/serrano-common/twrp.mk
+-include $(COMMON_PATH)/twrp.mk
 endif

Tab-3-snap/android_device_samsung_lt02lte-common

BoardConfigCommon.mk
@@ -21,6 +21,8 @@
 # definition file).
 #
 
+COMMON_PATH := device/samsung/lt02lte-common
+
 # Inherit from common msm8930
 -include device/samsung/msm8930-common/BoardConfigCommon.mk
 
@@ -40,7 +42,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
 # Recovery
 LZMA_RAMDISK_TARGETS := recovery
-TARGET_RECOVERY_FSTAB := device/samsung/lt02lte-common/rootdir/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/fstab.qcom
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -54,8 +56,9 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Dexpreopt To lower the first boot time
-WITH_DEXPREOPT := true
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth
 
 # Liblights
 TARGET_PROVIDES_LIBLIGHT := true
@@ -81,8 +84,8 @@ BOARD_RIL_CLASS := ../../../device/samsung/lt02lte-common/ril/
 BOARD_USES_QC_TIME_SERVICES := true
 
 # CMHW
-BOARD_HARDWARE_CLASS += device/samsung/lt02lte-common/cmhw
+BOARD_HARDWARE_CLASS += $(COMMON_PATH)/cmhw
 
 ifeq ($(WITH_TWRP),true)
--include device/samsung/lt02lte-common/twrp.mk
+-include $(COMMON_PATH)/twrp.mk
 endif